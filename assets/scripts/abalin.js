03f5761b08bd97e2e17e12679d2e88e23361195d
MyNa Do <myna.do@mahoyo.com>
2020-01-12 01:05:59 +0100

    added a new api address to get today's name by timezone and country

--- assets/scripts/abalin.js
+++ assets/scripts/abalin.js
@@ -2,7 +2,6 @@
  * Abalin API
  * 
  */
-// /namedays?country=us&month=7&day=15
 
 const baseUrl = "https://api.abalin.net";
 
@@ -20,6 +19,17 @@ const getDateByName = async (name, country = "us") => {
 const getNameByDate = async (country, month, day) => {
     // get name by date from Abalin API
     const response = await fetch(`${baseUrl}/namedays?country=${country}&month=${month}&day=${day}`);
+
+    // convert response from JSON
+    const data = await response.json();   
+
+    // return date
+    return data;
+};
+
+const getTodaysNameByTimezoneAndCountry = async (timezone, country) => {
+    // get today's name by timezone and country
+    const response = await fetch(`${baseUrl}/today?timezone=${timezone}&country=${country}`);
 
     // convert response from JSON
     const data = await response.json();   
